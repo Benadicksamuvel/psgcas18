b04c9d2e56b17aa29b999b26a6442b4a36f47340
Benadicksamuvel <136051110+Benadicksamuvel@users.noreply.github.com>
2024-04-12 11:03:15 +0530

    Create psg.c

---
+++ psg.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int num, originalNum, remainder, resu
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+       // remainder contains the last digit
+        remainder = originalNum 
+        
+       result += remainder * remainder * remainder;
+        
+       // removing last digit from the orignal number
+       originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num)
+
+    return 0;
+}
